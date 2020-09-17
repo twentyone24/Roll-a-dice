116c144c4acf58ab1a25a5576290b4f8eb86da62
Navemics <navemics@gmail.com>
2020-09-17 18:24:47 +0530

    Initial Commit

---
+++ Roll A Dice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Roll A Dice
+//
+//  Created by NAVEEN MADHAN on 9/17/20.
+//  Copyright © 2020 Swift Guru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
